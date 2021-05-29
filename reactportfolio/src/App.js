a7280d1128dd42cbe1bb805791ebb6fa97e1732f
Shon Hatcher <shonhatcher@hotmail.com>
2021-05-29 01:21:12 -0500

    Homepage complete

---
+++ reactportfolio/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/Home.js'
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route path = '/' exact component = {Home}/> 
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ reactportfolio/src/components/HeroSection/Hero.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Hero.css'; 
+
+
+function Hero() {
+    return (
+        <>
+        <div>
+            <img src = '/images/homeBackground.jpg' alt = "digital background"></img>
+            <h1> Full-Stack Web Developer</h1>
+            <button type = "button" class = "btn btn-warning btn-lg">
+                Tell Me More
+            </button>
+        
+        </div>
+        </>
+    )
+}
+
+export default Hero; 
+
